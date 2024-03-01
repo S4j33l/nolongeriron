95d1b2cf48f0e32d6a5111152dfd706c6e9176f2
S4j331 <sajeeltipu4903@gmail.com>
2024-03-01 17:01:04 +0500

    fixing

--- src/patient.rs
+++ src/patient.rs
@@ -1,3 +1,5 @@
+use core::panic;
+use std::io;
 #[derive(Clone)]
 pub struct Patient {
     pub name: String,
@@ -22,5 +24,40 @@ impl Patient {
         println!("The diagnosis of the patient is: {}", self.diagnosis);
         println!("The weight of the patient is: {}", self.weight);
         println!("The height of the patient is: {}", self.height);
+    }
+    pub fn change_patient_information(&mut self) {
+        println!("What do you want to change? (name, age, diagnosis, weight, height)");
+        let mut change = String::new();
+        io::stdin().read_line(&mut change).unwrap();
+        change = change.trim().to_string();
+        let change_for_match = &change[..];
+        match change_for_match {
+            "Name" | "name" => Self::change_name(change_for_match),
+            "Age" | "age" => Self::change_age(change_for_match).to_string(),
+            "Diagnosis" | "diagnosis" => Self::change_diagnosis(change_for_match),
+            "Weight" | "weight" => Self::change_weight(change_for_match).to_string(),
+            "Height" | "height" => Self::change_height(change_for_match).to_string(),
+            _ => panic!(),
+        };
+    }
+    fn change_name(mut name: &str) -> String {
+        println!("Please enter the new name of the patient: ");
+        let mut new_name = String::new();
+        io::stdin().read_line(&mut new_name).unwrap();
+        new_name = new_name.trim().to_string();
+        name = &new_name;
+        name.to_string()
+    }
+    fn change_age(age: &str) -> u32 {
+        age.to_string
+    }
+    fn change_diagnosis(diagnosis: &str) -> String {
+        diagnosis.to_string()
+    }
+    fn change_weight(weight: &str) -> f32 {
+        weight.to_string()
+    }
+    fn change_height(height: &str) -> f32 {
+        height.to_string()
     }
 }
